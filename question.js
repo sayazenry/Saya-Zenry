02c3ea19894588c334171e62675d1307b6ccb045
sayazenry <sayazenry@gmail.com>
2025-02-22 01:50:04 +0700

    Grammar

---
+++ question.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which tense is used to describe an action that started in the past and is still ongoing?",
+        answer: "C. Present Perfect Continuous",
+        options: [
+            "A. Past Continuous",
+            "B. Present Perfect",
+            "C. Present Perfect Continuous",
+            "D. Past Perfect Continuous"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Identify the error in the following sentence: 'By the time you arrived, we had been waiting for two hours.'",
+        answer: "D. No error",
+        options: [
+            "A. 'By the time' should be replaced with 'When'",
+            "B. 'Had been waiting' should be replaced with 'Were waiting'",
+            "C. 'Arrived' should be replaced with 'Had arrived'",
+            "D. No error"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which tense is typically used in formal news reports to describe past events?",
+        answer: "D. Past Simple",
+        options: [
+            "A. Past Perfect",
+            "B. Present Simple",
+            "C. Present Perfect",
+            "D. Past Simple"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Choose the correct sentence:",
+        answer: "B. 'She said that she had finished her work.'",
+        options: [
+            "A. 'She said that she has finished her work.'",
+            "B. 'She said that she had finished her work.'",
+            "C. 'She said that she was finishing her work.'",
+            "D. 'She said that she finishes her work.'"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which sentence correctly expresses a future plan that was decided before speaking?",
+        answer: "B. 'I am going to the conference next week.'",
+        options: [
+            "A. 'I will go to the conference next week.'",
+            "B. 'I am going to the conference next week.'",
+            "C. 'I go to the conference next week.'",
+            "D. 'I will be going to the conference next week.'",
+
+        ]
+    }
+]
